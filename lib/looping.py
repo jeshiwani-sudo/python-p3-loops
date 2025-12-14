ae912de906d522e36ddbd46b6e509151266acfe9
James Eshiwani <james.eshiwani@student.moringaschool.com>
2025-12-14 22:06:15 +0300

    Loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square = [num * num for num in int_list]
+    return square
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    a = 1
+    while a <= 100:
+        if a % 3 == 0 and a % 5 == 0:
+            print("FizzBuzz")
+        elif a % 3 == 0:
+            print("Fizz")
+        elif a % 5 == 0:
+            print("Buzz")
+        else:
+            print(a) 
+        a += 1
